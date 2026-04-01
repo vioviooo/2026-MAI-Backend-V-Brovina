ffcc7d7b73c2c323f98e4d0b37317eed4d4088f3
Victoria Brovina <117751176+vioviooo@users.noreply.github.com>
2026-04-01 17:42:17 +0300

    git add example usage

---
+++ example.py
@@ -0,0 +1,17 @@
+from cache_project import LRUCache
+
+def main():
+    cache = LRUCache(100)
+    
+    cache.set('Jesse', 'Pinkman')
+    cache.set('Walter', 'White')
+    cache.set('Jesse', 'James')
+    
+    print(f"Get Jesse: {cache.get('Jesse')}")  # * должен вывести 'James'
+    
+    cache.rem('Walter')
+    
+    print(f"Get Walter: {cache.get('Walter')}")  # * должен вывести return ''
+
+if __name__ == "__main__":
+    main()
